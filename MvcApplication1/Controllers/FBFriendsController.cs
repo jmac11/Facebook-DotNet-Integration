43fd9acb7a8e99ace356e3d0d465d63220e3c4d6
Ambarish Ganguly <ambarish.ganguly@gmail.com>
2013-07-03 19:47:43 +0530

    Facebook Dot Net Integration

---
+++ MvcApplication1/Controllers/FBFriendsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+using Facebook;
+using System.Collections;
+using Newtonsoft.Json.Linq;
+
+namespace MvcApplication1.Controllers
+{
+    public class FBFriendsController : Controller
+    {
+        //
+        // GET: /FBFriends/
+
+        public ActionResult Index()
+        {
+            
+             if (Session["facebooktoken"] != null)
+            {
+                string myAccessToken = Session["facebooktoken"].ToString();         
+                FacebookClient client = new FacebookClient(myAccessToken);         
+
+                // Friends List
+               // var friendListData = client.Get("/me/friends");
+
+                // Likes List
+                var friendListData = client.Get("/me/likes");
+
+                JObject friendListJson = JObject.Parse(friendListData.ToString());
+
+                ArrayList fbUsers = new ArrayList();
+                foreach (var friend in friendListJson["data"].Children())             
+                {   
+                    FbUser fbUser = new FbUser();
+                    fbUser.Id = friend["category"].ToString().Replace("\"", "");
+                    fbUser.Name = friend["name"].ToString().Replace("\"", "");
+
+                    fbUsers.Add(fbUser);
+                }
+
+                ViewData["FBFriends"] = fbUsers;
+             }
+
+
+
+             return View(ViewData["FBFriends"]);
+        }
+
+        //
+        // GET: /FBFriends/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /FBFriends/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /FBFriends/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /FBFriends/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /FBFriends/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /FBFriends/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /FBFriends/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using MvcApplication1.Models;
+using Facebook;
+using System.Collections;
+using Newtonsoft.Json.Linq;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+
+
+           //List<Object> education ;
+
+           //var msg = "";
+
+
+           //if (Session["facebooktoken"] != null)
+           //{
+           //    var client = new Facebook.FacebookClient(Session["facebooktoken"].ToString());
+
+           //    dynamic response = client.Get("me", new { fields = "education" });
+
+           //    if (response.ContainsKey("education"))
+           //    {
+           //        education = response["education"];
+
+           //        int count = education.Count();
+
+           //        for (int counter = 0; counter < count; counter++)
+           //        {
+
+           //            dynamic obj = education[counter];
+
+           //            dynamic obj2 = obj[0];
+
+           //            dynamic obj3 = obj2[1];
+
+           //            msg = msg + "<BR/> " + obj3.ToString();
+           //        }
+           //    }
+
+           //    msg = "Your education is  " + msg;
+
+           //  }
+
+           //ViewBag.Message =  msg;
+
+           if (Session["facebooktoken"] != null)
+           {
+               string myAccessToken = Session["facebooktoken"].ToString();
+               FacebookClient client = new FacebookClient(myAccessToken);
+
+               // Friends List
+               // var friendListData = client.Get("/me/friends");
+
+               // Likes List
+               var friendListData = client.Get("/me/likes");
+
+               JObject friendListJson = JObject.Parse(friendListData.ToString());
+
+               ArrayList fbUsers = new ArrayList();
+               foreach (var friend in friendListJson["data"].Children())
+               {
+                   FbUser fbUser = new FbUser();
+                   fbUser.Id = friend["category"].ToString().Replace("\"", "");
+                   fbUser.Name = friend["name"].ToString().Replace("\"", "");
+
+                   fbUsers.Add(fbUser);
+               }
+
+               ViewData["FBFriends"] = fbUsers;
+           }
+
+
+           return View(ViewData["FBFriends"]);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
